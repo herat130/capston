710577c38fa457bacc83c97104ea22e34e963a59
Herat.Dhruv <herat.dhruv@olx.com>
2021-12-13 23:44:09 +0100

    docker changes

--- my-app/run_docker.sh
+++ my-app/run_docker.sh
@@ -1,8 +1,7 @@
 echo "docker_run Param"
 echo $1
-echo $0
 docker build --tag=capston .
 docker image ls
 # docker run -p 3000:80 capston
-docker tag capston heratdhruv/capston
+docker tag capston heratdhruv/capston:$1
 docker push heratdhruv/capston:$1
